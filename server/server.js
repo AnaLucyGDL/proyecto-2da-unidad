24fcbf0454aecd59d0b73152819fd10832298a12
AnaLucyGDL <75144695+AnaLucyGDL@users.noreply.github.com>
2020-12-04 23:08:09 -0600

    ultimo comitt, ya matenme

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require('./config/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.send('<h1>Bienvenido a mi servidor REST</h1>');
+});
+
+app.use(require('./routes/usuario'));
+app.use(require('./routes/categoria'));
+app.use(require('./routes/login'));
+
+mongoose.connect('mongodb+srv://admin:blind2001@cluster0.et8gt.mongodb.net/cafeteria', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('El servidor esta en linea por el puerto ', process.env.PORT);
+});
